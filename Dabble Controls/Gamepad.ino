6f3f6f28b5a24770d4e92f8716f54a6f173fe5fb
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2024-10-05 22:40:37 -0700

    updated the controls' actions, needs the pins of the respective motors to be specified.

--- Dabble Controls/Gamepad.ino
+++ Dabble Controls/Gamepad.ino
@@ -2,10 +2,17 @@
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
+
+int motor1Power = 0;
+int motor2Power = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(250000);      // make sure your Serial Monitor is also set at this baud rate.
   Dabble.begin(9600);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
+  //Need the pin numbers for these motors
+  pinMode(motorControl1, OUTPUT);
+  pinMode(motorContorl2, OUTPUT);
 }
 
 void loop() {
@@ -34,21 +41,29 @@ void loop() {
   if (GamePad.isSquarePressed())
   {
     Serial.print("Square");
+    digitalWrite(motorControl2, HIGH);
+    delay(50);
   }
 
   if (GamePad.isCirclePressed())
   {
     Serial.print("Circle");
+    digitalWrite(motorControl1, HIGH);
+    delay(50);
   }
 
   if (GamePad.isCrossPressed())
   {
     Serial.print("Cross");
+    digitalWrite(motorControl2, LOW);
+    delay(20);
   }
 
   if (GamePad.isTrianglePressed())
   {
     Serial.print("Triangle");
+    digitalWrite(motorControl2, LOW);
+    delay(20);
   }
 
   if (GamePad.isStartPressed())
@@ -70,12 +85,31 @@ void loop() {
   Serial.print("Radius: ");
   Serial.print(b);
   Serial.print('\t');
-  float c = GamePad.getXaxisData();
+  float x = GamePad.getXaxisData();
   Serial.print("x_axis: ");
-  Serial.print(c);
+  Serial.print(x);
   Serial.print('\t');
-  float d = GamePad.getYaxisData();
+  float y = GamePad.getYaxisData();
   Serial.print("y_axis: ");
-  Serial.println(d);
+  Serial.println(y);
   Serial.println();
+if(abs(x) != 7){
+  motor2Power = int((2.20 ^ abs(x)));
+}else{
+  motor2Power = 255;
+}
+if(abs(y) ! = 7){
+  motor1Power = int((2.20 ^ abs(y)));
+}else{
+motor1Power = 255;
+}
+//will update with the correct pins and motors with their corresponding assignments.
+  analogWrite(motorControl, motor1Power);
+  analogWrite(motorControl, motor2Power);
+
+
+  
+
+
+
 }
