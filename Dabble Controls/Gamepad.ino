3025d2f82d1231a1c7418625c051377bfee39533
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2024-10-15 09:20:58 -0700

    code now works for two motors - change voltage and direction. still needs the pins numbers and configuration.

--- Dabble Controls/Gamepad.ino
+++ Dabble Controls/Gamepad.ino
@@ -3,8 +3,14 @@
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
 
-int motor1Power = 0;
-int motor2Power = 0;
+int motorLPower = 0;
+int motorLpin1;
+int motorLPWM;
+int motorLpin2;
+int motorRpin1;
+int motorRPWM;
+int motorRpin2;
+int motorRPower = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -81,7 +87,7 @@ void loop() {
   Serial.print("Angle: ");
   Serial.print(a);
   Serial.print('\t');
-  int b = GamePad.getRadius();
+  int r = GamePad.getRadius();
   Serial.print("Radius: ");
   Serial.print(b);
   Serial.print('\t');
@@ -93,23 +99,65 @@ void loop() {
   Serial.print("y_axis: ");
   Serial.println(y);
   Serial.println();
-if(abs(x) != 7){
-  motor2Power = int((2.20 ^ abs(x)));
-}else{
-  motor2Power = 255;
-}
-if(abs(y) ! = 7){
-  motor1Power = int((2.20 ^ abs(y)));
-}else{
-motor1Power = 255;
-}
-//will update with the correct pins and motors with their corresponding assignments.
-  analogWrite(motorControl, motor1Power);
-  analogWrite(motorControl, motor2Power);
+
+
+
+// as the x gets greater L power motor needs to be greater than R
+// as y gets greater 
+int motorDifference = 0;
+int motorPower = 0;
+
+
+if(y>0){
+  motorDifference = int((18 * x));
+  motorPower = int((39 * y));
+  //Both Motor Needs to be forward
+  if(y == 7){
+    analogWrite(motorLPWM, 255);
+    analogWrite(motorRPWM, 255);
+  }else if(x>0){
+  if(motorPower - motorDiffernce < 0){
+    //reverse motor R
+  }
+  analogWrite(motorLPWM, motorPower);
+  analogWrite(motorRPWM, abs(motorPower - motorDifference));
+  }
+  else{
+    if(motorPower - motorDifference < 0 ){
+      //reverse motor L
+    }
+    analogWrite(motorLPWM, abs(motorPower - motorDifference));
+    analogWrite(motorRPWM, motorPower);
+  }
+  }
+else{
+  //Both Motor Needs to be backward
+  motorDifference = int((18 * x));
+  motorPower = int((39 * abs(y)));
+  if(y == -7){
+    analogWrite(motorLPWM, 255);
+    analogWrite(motorRPWM, 255);
+  }else if(x>0){
+  if(motorPower - motorDiffernce < 0){
+    //reverse motor R from the upper control system
+  }
+  analogWrite(motorLPWM, motorPower);
+  analogWrite(motorRPWM, abs(motorPower - motorDifference));
+  }
+  else{
+    if(motorPower - motorDifference < 0 ){
+      //reverse motor L from the upper control system
+    }
+    analogWrite(motorLPWM, abs(motorPower - motorDifference));
+    analogWrite(motorRPWM, motorPower);
+  }
+  }
+ 
+
+
 
 
   
-
 
 
 }
